4bd8612005415d1cf64ca37e4b75d1ab5e92769c
Yash Kumar Rana <2200520100072@ietlucknow.ac.in>
2024-10-03 12:13:50 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -7,7 +7,17 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000, () =>{
+    console.log(`Server is running at port: ${process.env.PORT}`)
+    
+  })
+})
+.catch((err)=>{
+  console.log('MONGO DB CONNECTION FAILED !!!',err);
+  
+})
 
 //  import express from "express";
 //  const app = express()
